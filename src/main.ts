5fff01912c69e5debd79f97e5f816df80f73af9a
Radmir Gurkov <ragux0@gmail.com>
2025-05-03 01:18:44 +0200

    reconciler

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,13 @@ const root = document.getElementById("app")!;
 const element = createElement(
   "div",
   { id: "foo" },
-  createElement("a", null, "bar"),
-  createElement("b", null, "baz")
+  createElement("a", { href: "https://example.com" }, "bar"),
+  createElement("input", {
+    value: "test",
+    onChange: (e: Event) => {
+      console.log("changed", e);
+    },
+  })
 );
 
 render(element, root);
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,9 +1,11 @@
+export type PropsWithChildren = {
+  [key: string]: any;
+  children?: (Element | string)[];
+};
+
 export type Element = {
-  type: string;
-  props: {
-    [key: string]: any;
-    children?: (Element | string)[];
-  };
+  type?: string;
+  props?: PropsWithChildren;
 };
 
 export function createTextElement(text: string): Element {
@@ -14,6 +16,48 @@ export function createTextElement(text: string): Element {
       children: [],
     },
   };
+}
+
+function updateDom(dom: any, prevProps: any, nextProps: any) {
+  const isEvent = (key: string) => key.startsWith("on");
+  const isProperty = (key: string) => key !== "children" && !isEvent(key);
+  const isGone = (prev: any, next: any) => (key: string) => !(key in next);
+  const isNew = (prev: any, next: any) => (key: string) =>
+    prev[key] !== next[key];
+
+  // Remove old properties
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = "";
+    });
+
+  // Set new properties
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = nextProps[name];
+    });
+
+  // Remove old event listeners
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter((name) => !(name in nextProps) || isNew(prevProps, nextProps)(name))
+    .forEach((name) => {
+      const eventName = name.toLowerCase().substring(2);
+      dom.removeEventListener(eventName, prevProps[name]);
+    });
+
+  // Add new event listeners
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter((name) => !(name in prevProps) || isNew(prevProps, nextProps)(name))
+    .forEach((name) => {
+      const eventName = name.toLowerCase().substring(2);
+      dom.addEventListener(eventName, nextProps[name]);
+    });
 }
 
 export function createElement<Props = []>(
@@ -32,48 +76,143 @@ export function createElement<Props = []>(
   };
 }
 
-export function render(element: Element, container: HTMLElement | Text) {
+function createDom(element: any) {
   const dom =
-    element.type === "TEXT ELEMENT"
+    element.type == "TEXT_ELEMENT"
       ? document.createTextNode("")
       : document.createElement(element.type);
-  container.appendChild(dom);
 
-  const isProperty = (key: string) => key !== "children";
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach((name) => {
-      (dom as any)[name] = element.props[name];
-    });
+  updateDom(dom, {}, element.props);
 
-  element.props.children?.forEach((child: Element | string) => {
-    if (typeof child === "object") {
-      render(child, dom);
-    } else {
-      dom.textContent = child;
-    }
-  });
-
-  container.appendChild(dom);
+  return dom;
 }
 
-// let nextUnitOfWork = null;
+let nextUnitOfWork: any | null = null;
+let wipRoot: any | null = null;
+let currentRoot: any | null = null;
+let deletions: any[] = [];
 
-// function workLoop(deadline) {
-//   let shouldYield = false;
-//   while (nextUnitOfWork && !shouldYield) {
-//     nextUnitOfWork = performUnitOfWork();
-//     if (nextUnitOfWork) {
-//       shouldYield = deadline.timeRemaining() < 1;
-//     }
-//   }
-//   requestIdleCallback(workLoop);
-// }
+export function render(element: Element, container: HTMLElement | Text) {
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+    alternate: currentRoot,
+  };
+  deletions = [];
+  nextUnitOfWork = wipRoot;
+}
 
-function performUnitOfWork() {
-  // 1. Get the next unit of work
-  // 2. If there is no unit of work, return
-  // 3. If there is a unit of work, perform the work
-  // 4. If the work is done, mark the unit of work as completed
-  // 5. If there are more units of work, perform the next unit of work
+function commitWork(element: any) {
+  console.log("commit work", element);
+  if (!element) {
+    return;
+  }
+  const domParent = element.parent?.dom ?? null;
+  if (element.effectTag === "PLACEMENT" && element.dom) {
+    domParent?.appendChild(element.dom);
+  } else if (element.effectTag === "UPDATE" && element.dom) {
+    updateDom(element.dom, element.alternate.props, element.props);
+  } else if (element.effectTag === "DELETION" && element.dom) {
+    domParent?.removeChild(element.dom);
+  }
+  commitWork(element.child);
+  commitWork(element.sibling);
+}
+
+function commitRoot() {
+  // add nodes to dom
+  console.log("commit root", wipRoot);
+  deletions.forEach(commitWork);
+  commitWork(wipRoot.child);
+  currentRoot = wipRoot;
+  wipRoot = null;
+}
+
+function workLoop(deadline: IdleDeadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork) ?? null;
+    if (nextUnitOfWork) {
+      shouldYield = deadline.timeRemaining() < 1;
+    }
+  }
+  if (!nextUnitOfWork && wipRoot) {
+    console.log("commit root");
+    commitRoot();
+  }
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function reconcileChildren(element: any, elements: any[]) {
+  let index = 0;
+  let oldFiber = element.alternate && element.alternate.child;
+  let prevSibling: any = null;
+
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index];
+    let newFiber = null;
+
+    const sameType = oldFiber && element && element.type == oldFiber.type;
+
+    if (sameType) {
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: element,
+        alternate: oldFiber,
+        effectTag: "UPDATE",
+      };
+    }
+    if (element && !sameType) {
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: element,
+        alternate: null,
+        effectTag: "PLACEMENT",
+      };
+    }
+    if (oldFiber && !sameType) {
+      oldFiber.effectTag = "DELETION";
+      deletions.push(oldFiber);
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
+
+    if (index === 0) {
+      element.child = newFiber;
+    } else if (element) {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+}
+function performUnitOfWork(element: any) {
+  if (!element.dom) {
+    element.dom = createDom(element);
+  }
+
+  const elements = element.props.children;
+  reconcileChildren(element, elements);
+
+  if (element.child) {
+    return element.child;
+  }
+  let nextElementSibling = element;
+  while (nextElementSibling) {
+    if (nextElementSibling.sibling) {
+      return nextElementSibling.sibling;
+    }
+    nextElementSibling = nextElementSibling.parent;
+  }
 }
